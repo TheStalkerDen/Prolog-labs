04ef9c13d5b63454b7f4f8cdba7c1b49e7f421f2
Den Ermol <denisermolenko17@gmail.com>
2021-03-18 23:56:27 +0200

    Solved task G

---
+++ taskG.pl
@@ -0,0 +1,5 @@
+split(_,[],[],[]):-!.
+split(M, [H|T],[H|L1],L2):- H < M, split(M,T,L1,L2).
+split(M, [H|T],L1,[H|L2]):- H > M, split(M,T,L1,L2).
+split(M, [H|T],L1,L2):- H =:= M, split(M,T,L1,L2).
+split(Middle, L, L1, L2):-split(Middle,L,L1,L2).
